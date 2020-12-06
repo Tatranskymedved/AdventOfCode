b0c1dff6c11a8524a5e86441ecc650b9b31e1ada
Jan Urbanec <UrbanecJ@seznam.cz>
2020-12-06 10:24:38 +0100

    WIP: Day 6 - first part without working IDE.

--- AdventOfCode/AOC.2020/Days/Day06/Day6.cs
+++ AdventOfCode/AOC.2020/Days/Day06/Day6.cs
@@ -10,12 +10,52 @@ namespace AOC._2020.Days
         public override int Main_Int32()
         {
             var path = @".\Days\Day06\input.txt";
+            var str = System.IO.File.ReadAllText(path);
 
-            var str = System.IO.File.ReadAllLines(path);
-            var charsToRemove = new char[] { ' ', ':', '-' };
+            var factory = new GroupFactory();
+            var groups = factory.ParseGroups(str);
 
-            //var values = str.Select();
-            return 0;
+            return groups.Sum(g => g.Count);
+        }
+    }
+
+    internal class GroupFactory
+    {
+        static string charsToSplitGroups = "\n\n";
+        public List<Group> ParseGroups(string inputLines)
+        {
+            var groups = inputLines.Split(charsToSplitGroups);
+            var groupsList = new List<Group>();
+            foreach (var groupLine in groups)
+            {
+                groupsList.Add(new Group(groupLine));
+            }
+
+            return groupsList;
+        }
+    }
+
+    internal class Group
+    {
+        static string charsToSplitPeople = "\n";
+        Dictionary<char, int> answers = new Dictionary<char, int>();
+        public int Count => answers.Count(a => a.Count());
+
+        public Group(string groupLine)
+        {
+            var peopleLines = groupLine.Split(charsToSplitPeople);
+
+            foreach (var personLine in peopleLines)
+            {
+                foreach (char answer in personLine)
+                {
+                    if(answers.TryGetKey(answer, out int count))
+                    {
+                        answers[answer] = count++;
+                    }
+                    answers.Add(answer, 1);
+                }
+            }
         }
     }
 }
--- AdventOfCode/AOC.2020/Program.cs
+++ AdventOfCode/AOC.2020/Program.cs
@@ -7,7 +7,7 @@ namespace AOC._2020
     {
         static void Main(string[] args)
         {
-            var day = new Day5();
+            var day = new Day6();
             Console.WriteLine(day.Main());
 
             Console.ReadLine();
