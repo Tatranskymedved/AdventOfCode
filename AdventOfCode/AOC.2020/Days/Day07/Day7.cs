fed578da97a042d687f119b6396a6433b12e9065
jan.urbanec <jan.urbanec@adacta-fintech.com>
2020-12-07 07:47:40 +0100

    Day 7 part 1

--- AdventOfCode/AOC.2020/Days/Day07/Day7.cs
+++ AdventOfCode/AOC.2020/Days/Day07/Day7.cs
@@ -12,10 +12,96 @@ namespace AOC._2020.Days
             var path = @".\Days\Day07\input.txt";
 
             var str = System.IO.File.ReadAllLines(path);
-            var charsToRemove = new char[] { ' ', ':', '-' };
 
-            //var values = str.Select();
-            return 0;
+            var bc = new BagCollector();
+            bc.Parse(str);
+
+            return bc.GetBagsThatContains("shiny gold").Count();
+        }
+    }
+
+    /// <summary>
+    /// For part 1
+    /// </summary>
+    class BagCollector
+    {
+        public List<Bag> Bags = new List<Bag>();
+        
+        public void Parse(string[] lines)
+        {
+            foreach (var line in lines)
+            {
+                Bags.Add(new Bag(line));
+            }
+        }
+
+        public HashSet<string> GetBagsThatContains(string value)
+        {
+            var result = new HashSet<string>();
+
+            ReccursiveGetBagsThatContains(value, ref result);
+
+            return result;
+        }
+
+        public bool ReccursiveGetBagsThatContains(string value, ref HashSet<string> hashSetOfBags)
+        {
+            if (hashSetOfBags == null) hashSetOfBags = new HashSet<string>();
+
+            bool foundNew = false;
+            foreach (var bag in Bags)
+            {
+                var foundNewBitStepUp = bag.GetParentsThatContains(value, hashSetOfBags);
+                if (foundNewBitStepUp)
+                    foundNew = foundNewBitStepUp;
+            }
+
+            while(foundNew)
+            {
+                foundNew = false;
+
+                var copiedBags = hashSetOfBags.ToArray();
+                foreach (var bag in copiedBags)
+                {
+                    var foundNewBitStepUp = this.ReccursiveGetBagsThatContains(bag,ref hashSetOfBags);
+                    if (foundNewBitStepUp)
+                        foundNew = foundNewBitStepUp;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    internal class Bag
+    {
+        static char[] splitChars = new char[] { ',', '.' };
+        public Dictionary<string, int> Store = new Dictionary<string, int>();
+        public string Name;
+
+        public Bag(string line)
+        {
+            var lineToTwoParts = line.Split(" bags contain ");
+            Name = lineToTwoParts[0];
+
+            if (lineToTwoParts[1].Equals("no other bags.")) return;
+
+            var containment = lineToTwoParts[1].Replace(" bags", "").Replace(" bag", "").Split(splitChars, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var singleInnerBagType in containment)
+            {
+                var singleBagSplit = singleInnerBagType.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                Store.Add(singleBagSplit[1] + " " + singleBagSplit[2], Convert.ToInt32(singleBagSplit[0]));
+            }
+        }
+
+        public bool GetParentsThatContains(string value, HashSet<string> hashSetOfParents)
+        {
+            if (this.Store.ContainsKey(value) && !hashSetOfParents.Contains(Name))
+            {
+                hashSetOfParents.Add(Name);
+                return true;
+            }
+            return false;
         }
     }
 }
