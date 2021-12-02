81469463b00e85d7c20502851c033b80867342d0
Jan Urbanec <UrbanecJ@seznam.cz>
2021-12-02 07:15:50 +0100

    Day 2

--- AdventOfCode/AOC.2021/Days/Day02/Day2.cs
+++ AdventOfCode/AOC.2021/Days/Day02/Day2.cs
@@ -14,13 +14,35 @@ namespace AOC._2021.Days
             var str = System.IO.File.ReadAllLines(path);
             var charsToRemove = new char[] { ' ', ':', '-' };
 
+            int horizontalPosition = 0, depth = 0, aim = 0;
+
             var values = str.Select(line =>
             {
                 var row = line.Split(charsToRemove, StringSplitOptions.RemoveEmptyEntries);
                 return row;
+            }).ToList();
+
+            values.ForEach(a =>
+            {
+                int val = Int32.Parse(a[1]);
+                switch (a[0])
+                {
+                    case "forward":
+                        horizontalPosition += val;
+                        depth += aim * val;
+                        break;
+                    case "down":
+                        aim += val;
+                        break;
+                    case "up":
+                        aim -= val;
+                        break;
+                    default:
+                        throw new Exception("unknown");
+                }
             });
 
-            return 0;
+            return horizontalPosition * depth;
         }
     }
 }
