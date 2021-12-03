abf3b3301d671e6062f428bc665d17e6f09fceee
Jan Urbanec <UrbanecJ@seznam.cz>
2021-12-03 08:11:55 +0100

    Day 3

--- AdventOfCode/AOC.2021/Days/Day03/Day3.cs
+++ AdventOfCode/AOC.2021/Days/Day03/Day3.cs
@@ -7,20 +7,102 @@ namespace AOC._2021.Days
 {
     class Day3 : ADay
     {
+        int bitSize;
+
         public override double Main_Double()
         {
             var path = @".\Days\Day03\input.txt";
+            //var path = @".\Days\Day03\myInput.txt";
 
-            var str = System.IO.File.ReadAllLines(path);
-            var charsToRemove = new char[] { ' ', ':', '-' };
+            var str = System.IO.File.ReadAllLines(path).ToList();
+            bitSize = str.First().Length;
 
-            var values = str.Select(line =>
+            //Part 1
+            //var array = new int[bitSize];
+            //for (int i = 0; i < str.Length; i++)
+            //{
+            //    var line = str[i];
+            //    for (int j = 0; j < bitSize; j++)
+            //    {
+            //        if (line[j] == '1')
+            //            array[j]++;
+            //    }
+            //}
+
+            //int mostCommon = 0;
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    if (array[i] > (str.Length / 2))
+            //    {
+            //        mostCommon |= 1 << (array.Length - i - 1);
+            //    }
+            //}
+            //int leastCommon = Convert.ToInt32(Math.Pow(2, array.Length)) - mostCommon - 1;
+
+            //return mostCommon * leastCommon;
+
+            //Part 2
+            var oxygen = convertFromStringToInt(removeFromList(str.ToList(), true));
+            var CO2 = convertFromStringToInt(removeFromList(str, false));
+            return oxygen * CO2;
+        }
+
+        public int convertFromStringToInt(string number)
+        {
+            int result = 0;
+            for (int i = 0; i < number.Length; i++)
             {
-                var row = line.Split(charsToRemove, StringSplitOptions.RemoveEmptyEntries);
-                return row;
-            });
+                result |= (number[i] == '1' ? 1 : 0)  << (number.Length - i - 1);
+            }
+            return result;
+        }
 
-            return 0;
+
+        public string removeFromList(List<string> list, bool isMostCommonValue)
+        {
+            for (int j = 0; j < bitSize; j++)
+            {
+                double count = list.Count() / 2d;
+                int one = list.Count(a => a[j] == '1');
+                if (isMostCommonValue)
+                {
+                    if (one < count)
+                    {
+                        list.Where(a => a[j] == '1').ToList().ForEach(a => list.Remove(a));
+                    }
+                    else if(one == count)
+                    {
+                        list.Where(a => a[j] == '0').ToList().ForEach(a => list.Remove(a));
+                    }
+                    else
+                    {
+                        list.Where(a => a[j] == '0').ToList().ForEach(a => list.Remove(a));
+                    }
+                }
+
+                if (isMostCommonValue == false)
+                {
+                    if (one > count)
+                    {
+                        list.Where(a => a[j] == '1').ToList().ForEach(a => list.Remove(a));
+                    }
+                    else if (one == count)
+                    {
+                        list.Where(a => a[j] == '1').ToList().ForEach(a => list.Remove(a));
+                    }
+                    else
+                    {
+                        list.Where(a => a[j] == '0').ToList().ForEach(a => list.Remove(a));
+                    }
+                }
+
+
+
+                if (list.Count() == 1)
+                    return list.First();
+            }
+
+            return "none";
         }
     }
 }
--- AdventOfCode/AOC.2021/Program.cs
+++ AdventOfCode/AOC.2021/Program.cs
@@ -11,7 +11,7 @@ namespace AOC._2021
     {
         static void Main(string[] args)
         {
-            var day = new Day2();
+            var day = new Day3();
             Console.WriteLine(day.Main());
 
             Console.ReadLine();
