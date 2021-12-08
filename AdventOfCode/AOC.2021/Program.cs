22f0a4c4191f006abf3166624a4acb363b8a7879
Jan Urbanec <UrbanecJ@seznam.cz>
2021-12-08 08:06:13 +0100

    Day 8

--- AdventOfCode/AOC.2021/Program.cs
+++ AdventOfCode/AOC.2021/Program.cs
@@ -11,7 +11,7 @@ namespace AOC._2021
     {
         static void Main(string[] args)
         {
-            var day = new Day7();
+            var day = new Day8();
             Console.WriteLine(day.Main());
 
             Console.ReadLine();
