1ab57e1853535bd273096a2dfbbeb271b0a25a62
Jan Urbanec <UrbanecJ@seznam.cz>
2021-12-10 07:27:52 +0100

    Day 10

--- AdventOfCode/AOC.2021/Days/Day10/Day10.cs
+++ AdventOfCode/AOC.2021/Days/Day10/Day10.cs
@@ -13,14 +13,130 @@ namespace AOC._2021.Days
 
             var str = System.IO.File.ReadAllLines(path);
             var charsToRemove = new char[] { ' ', ':', '-' };
+            int sum = 0;
 
+            List<string> justIncompleteLines = new List<string>();
             var values = str.Select(line =>
             {
-                var row = line.Split(charsToRemove, StringSplitOptions.RemoveEmptyEntries);
-                return row;
-            });
+                var rows = line.Split(charsToRemove, StringSplitOptions.RemoveEmptyEntries);
 
-            return 0;
+
+                for (int i = 0; i < rows.Length; i++)
+                {
+                    var q = new Stack<char>();
+                    string row = rows[i];
+                    bool wasIncorrect = false;
+
+
+                    for (int j = 0; j < row.Length; j++)
+                    {
+                        var c = row[j];
+
+                        if (c.IsOpen())
+                        {
+                            q.Push(c);
+                        }
+                        else
+                        {
+                            var d = q.Pop();
+                            if (d == '(')
+                            {
+                                if (c != ')')
+                                {
+                                    sum += c.GetScore();
+                                    wasIncorrect = true;
+                                    break;
+                                }
+                            }
+                            if (d == '[')
+                            {
+                                if (c != ']')
+                                {
+                                    sum += c.GetScore();
+                                    wasIncorrect = true;
+                                    break;
+                                }
+                            }
+                            if (d == '{')
+                            {
+                                if (c != '}')
+                                {
+                                    sum += c.GetScore();
+                                    wasIncorrect = true;
+                                    break;
+                                }
+                            }
+                            if (d == '<')
+                            {
+                                if (c != '>')
+                                {
+                                    sum += c.GetScore();
+                                    wasIncorrect = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    if (wasIncorrect == false)
+                    {
+                        justIncompleteLines.Add(row);
+                    }
+                }
+
+
+
+                return rows;
+            }).ToList();
+
+            var scores = new List<double>();
+            for (int i = 0; i < justIncompleteLines.Count; i++)
+            {
+                var score = 0d;
+                var q = new Stack<char>();
+                var line = justIncompleteLines[i];
+
+                for (int j = 0; j < line.Length; j++)
+                {
+                    char c = line[j];
+
+                    if (c.IsOpen())
+                        q.Push(c);
+                    else
+                        q.Pop();
+                }
+
+                while (q.TryPop(out char d))
+                {
+                    if (d == '(')
+                        score = (score * 5) + 1;
+                    if (d == '[')
+                        score = (score * 5) + 2;
+                    if (d == '{')
+                        score = (score * 5) + 3;
+                    if (d == '<')
+                        score = (score * 5) + 4;
+                }
+                scores.Add(score);
+            }
+
+            scores.Sort();
+            var middleScore = scores[scores.Count / 2 ];
+
+            return middleScore;
+        }
+    }
+
+    public static class ExtChar
+    {
+        public static bool IsOpen(this char c)
+        {
+            return c == '(' || c == '[' || c == '{' || c == '<';
+        }
+        public static int GetScore(this char c)
+        {
+            return c == ')' ? 3 :
+                c == ']' ? 57 :
+                c == '}' ? 1197 : 25137;
         }
     }
 }
