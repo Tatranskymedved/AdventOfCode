7c17d322009095cdf593b6eebbf4c0dfade1657e
Jan Urbanec <UrbanecJ@seznam.cz>
2021-12-11 07:42:04 +0100

    Day 11

--- AdventOfCode/AOC.2021/Days/Day11/Day11.cs
+++ AdventOfCode/AOC.2021/Days/Day11/Day11.cs
@@ -14,13 +14,158 @@ namespace AOC._2021.Days
             var str = System.IO.File.ReadAllLines(path);
             var charsToRemove = new char[] { ' ', ':', '-' };
 
-            var values = str.Select(line =>
+
+            int maxSteps = 10000;
+            var b = new DumboOctopusBoard(str);
+            int step = b.MakeSteps(maxSteps);
+
+            //return b.FlashCounter;
+            return step;
+        }
+    }
+
+    class DumboOctopusBoard
+    {
+        int width = 0;
+        int height = 0;
+        DumboOctopus[,] array;
+
+        public int FlashCounter { get; set; } = 0;
+
+        public DumboOctopusBoard(string[] str)
+        {
+            width = str.First().Length;
+            height = str.Length;
+            array = new DumboOctopus[height, width];
+
+            for (int y = 0; y < str.Length; y++)
             {
-                var row = line.Split(charsToRemove, StringSplitOptions.RemoveEmptyEntries);
-                return row;
-            });
+                var row = str[y];
+                for (int x = 0; x < row.Length; x++)
+                {
+                    array[y, x] = new DumboOctopus(Convert.ToInt32(row[x].ToString()));
+                }
+            }
+
+        }
+
+        public int MakeSteps(int maxSteps)
+        {
+            for (int i = 0; i < maxSteps; i++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        MakeStepForOctopuses(y, x);
+                    }
+                }
+
+                bool anyNotFlashed = false;
+
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        if (array[y, x].HasFlashed == false)
+                        {
+                            anyNotFlashed = true;
+                            break;
+                        }
+                    }
+                    if (anyNotFlashed)
+                    {
+                        break;
+                    }
+                }
+                if (anyNotFlashed == false) //All flashinig together
+                    return i + 1;
+
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        array[y, x].Reset();
+                    }
+                }
+            }
 
             return 0;
+        }
+
+
+        private bool MakeStepForOctopuses(int y, int x)
+        {
+            var hasFlashed = array[y, x].MakeStepAndReturnIfFlashed();
+            if (hasFlashed == false) return false;
+
+            FlashCounter++;
+
+            if (y > 0)
+            {
+                MakeStepForOctopuses(y - 1, x);
+                if (x > 0)
+                {
+                    MakeStepForOctopuses(y - 1, x - 1);
+                }
+                if (x < width - 1)
+                {
+                    MakeStepForOctopuses(y - 1, x + 1);
+                }
+            }
+            if (y < height - 1)
+            {
+                MakeStepForOctopuses(y + 1, x);
+                if (x > 0)
+                {
+                    MakeStepForOctopuses(y + 1, x - 1);
+                }
+                if (x < width - 1)
+                {
+                    MakeStepForOctopuses(y + 1, x + 1);
+                }
+            }
+
+            if (x > 0)
+            {
+                MakeStepForOctopuses(y, x - 1);
+            }
+            if (x < width - 1)
+            {
+                MakeStepForOctopuses(y, x + 1);
+            }
+
+            return true;
+        }
+    }
+
+    class DumboOctopus
+    {
+        public bool HasFlashed { get; set; } = false;
+        public int Value { get; set; }
+
+        public DumboOctopus(int value)
+        {
+            Value = value;
+        }
+
+        public bool MakeStepAndReturnIfFlashed()
+        {
+            Value++;
+            if (Value > 9 && (HasFlashed == false))
+            {
+                HasFlashed = true;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Reset()
+        {
+            HasFlashed = false;
+            if (Value > 9)
+                Value = 0;
         }
     }
 }
