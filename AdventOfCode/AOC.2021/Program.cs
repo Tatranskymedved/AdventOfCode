2c5fb57a7bb3c91fdd729fb59dbba64a1ca212d8
Jan Urbanec <UrbanecJ@seznam.cz>
2021-12-14 15:14:36 +0100

    Day 14

--- AdventOfCode/AOC.2021/Program.cs
+++ AdventOfCode/AOC.2021/Program.cs
@@ -11,7 +11,7 @@ namespace AOC._2021
     {
         static void Main(string[] args)
         {
-            var day = new Day13();
+            var day = new Day14();
             Console.WriteLine(day.Main());
 
             Console.ReadLine();
