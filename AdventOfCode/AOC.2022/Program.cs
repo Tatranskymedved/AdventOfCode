ba5ad41384faf077b9eddc206ef95c774a6e8b33
Jan Urbanec <UrbanecJ@seznam.cz>
2022-12-06 12:08:22 +0100

    Day 6

--- AdventOfCode/AOC.2022/Program.cs
+++ AdventOfCode/AOC.2022/Program.cs
@@ -11,7 +11,7 @@ namespace AOC._2022
     {
         static void Main(string[] args)
         {
-            var day = new Day05();
+            var day = new Day06();
             Console.WriteLine(day.Main());
 
             Console.ReadLine();
