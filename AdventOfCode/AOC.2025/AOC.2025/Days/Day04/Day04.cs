15a6a5b30e22054d27b2aee32346d88516bec79c
Jan Urbanec <UrbanecJ@seznam.cz>
2025-12-04 07:36:44 +0100

    Day 04 [2025]

---
+++ AdventOfCode/AOC.2025/AOC.2025/Days/Day04/Day04.cs
@@ -0,0 +1,119 @@
+﻿using AOC.Common.Days;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Transactions;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace AOC._2025.Days
+{
+    class Day04 : ADay
+    {
+        public override double Main_Double()
+        {
+            var path = @".\Days\Day04\input.txt";
+
+            var str = System.IO.File.ReadAllLines(path);
+
+            int width = str[0].Length, height = str.Length;
+
+            Node[,] arr = new Node[height, width];
+            for (int iY = 0; iY < height; iY++)
+            {
+                string line = str[iY];
+                for (int iX = 0; iX < width; iX++)
+                {
+                    arr[iX, iY] = new Node(c: line[iX]);
+                }
+            }
+
+            int sumOfMoveableRolls = 0;
+            for (int iY = 0; iY < height; iY++)
+            {
+                for (int iX = 0; iX < width; iX++)
+                {
+                    if (arr[iX, iY].C != '@') continue;
+
+                    int numOfRollsAround = 0;
+                    try { if (arr[iX - 1, iY - 1].C == '@') { ++numOfRollsAround; } } catch { }
+                    try { if (arr[iX - 1, iY + 0].C == '@') { ++numOfRollsAround; } } catch { }
+                    try { if (arr[iX - 1, iY + 1].C == '@') { ++numOfRollsAround; } } catch { }
+                    try { if (arr[iX + 0, iY - 1].C == '@') { ++numOfRollsAround; } } catch { }
+                    try { if (arr[iX + 0, iY + 1].C == '@') { ++numOfRollsAround; } } catch { }
+                    try { if (arr[iX + 1, iY - 1].C == '@') { ++numOfRollsAround; } } catch { }
+                    try { if (arr[iX + 1, iY + 0].C == '@') { ++numOfRollsAround; } } catch { }
+                    try { if (arr[iX + 1, iY + 1].C == '@') { ++numOfRollsAround; } } catch { }
+
+                    if (numOfRollsAround < 4)
+                    {
+                        arr[iX, iY].CanBeAccessedByForklift = true;
+                        sumOfMoveableRolls++;
+                    }
+                }
+            }
+
+            int counterOfRemovedRolls = 0;
+            while (RunCleanup(ref arr, height, width, ref counterOfRemovedRolls)) ;
+
+
+            //Debug:
+            //Console.WriteLine();
+            //for (int iY = 0; iY < height; iY++)
+            //{
+            //    for (int iX = 0; iX < width; iX++)
+            //    {
+            //        Console.Write(arr[iX, iY].CanBeAccessedByForklift ? 'x' : arr[iX, iY].C);
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.WriteLine();
+
+
+
+
+            // Part 1
+            //return sumOfMoveableRolls;
+
+            return counterOfRemovedRolls;
+        }
+
+        bool RunCleanup(ref Node[,] arr, int height, int width, ref int counter)
+        {
+            bool wasAnythingCleaned = false;
+
+            for (int iY = 0; iY < height; iY++)
+            {
+                for (int iX = 0; iX < width; iX++)
+                {
+                    if (arr[iX, iY].C != '@') continue;
+
+                    int numOfRollsAround = 0;
+                    try { if (arr[iX - 1, iY - 1].C == '@') { ++numOfRollsAround; } } catch { }
+                    try { if (arr[iX - 1, iY + 0].C == '@') { ++numOfRollsAround; } } catch { }
+                    try { if (arr[iX - 1, iY + 1].C == '@') { ++numOfRollsAround; } } catch { }
+                    try { if (arr[iX + 0, iY - 1].C == '@') { ++numOfRollsAround; } } catch { }
+                    try { if (arr[iX + 0, iY + 1].C == '@') { ++numOfRollsAround; } } catch { }
+                    try { if (arr[iX + 1, iY - 1].C == '@') { ++numOfRollsAround; } } catch { }
+                    try { if (arr[iX + 1, iY + 0].C == '@') { ++numOfRollsAround; } } catch { }
+                    try { if (arr[iX + 1, iY + 1].C == '@') { ++numOfRollsAround; } } catch { }
+
+                    if (numOfRollsAround < 4)
+                    {
+                        arr[iX, iY].C = '.';
+                        wasAnythingCleaned = true;
+                        counter++;
+                    }
+                }
+            }
+            return wasAnythingCleaned;
+        }
+
+        public struct Node(char c, bool canBeAccessedByForklift = false)
+        {
+            public char C { get; set; } = c;
+            public bool CanBeAccessedByForklift { get; set; } = canBeAccessedByForklift;
+        }
+    }
+}
--- AdventOfCode/AOC.2025/AOC.2025/Program.cs
+++ AdventOfCode/AOC.2025/AOC.2025/Program.cs
@@ -2,7 +2,7 @@
 using AOC._2025;
 using AOC._2025.Days;
 
-var day = new Day03();
+var day = new Day04();
 Console.WriteLine(day.Main());
 
 Console.ReadLine();
